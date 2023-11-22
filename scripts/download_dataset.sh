0ff9ce3adb6c6c91b9ed5fc920c3a34a6828fa03
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-22 21:11:48 +0000

    Change ownership of dataset directory

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -9,4 +9,4 @@ rm -rf "tmp/datasets/${datasetName}.zip"
 wget -O "tmp/datasets/${datasetName}.zip" "https://codecrafters-coderbot-datasets.s3.amazonaws.com/${datasetName}.zip"
 unzip "tmp/datasets/${datasetName}.zip" -d "tmp/datasets/${datasetName}"
 
-chown -R $USER:$USER "tmp/datasets/${datasetName}"
+sudo chown -R $USER "tmp/datasets/${datasetName}"
