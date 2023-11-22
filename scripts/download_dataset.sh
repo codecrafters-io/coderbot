401e613f9b5519234c85dec05c5f2dce82159426
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-22 21:13:20 +0000

    Update download_dataset.sh to conditionally use quiet mode for wget

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -6,7 +6,13 @@ datasetName=$1
 mkdir -p tmp/datasets
 rm -rf "tmp/datasets/${datasetName}"
 rm -rf "tmp/datasets/${datasetName}.zip"
-wget -O "tmp/datasets/${datasetName}.zip" "https://codecrafters-coderbot-datasets.s3.amazonaws.com/${datasetName}.zip"
+
+if [ "$CI" = "true" ]; then
+    wget -q -O "tmp/datasets/${datasetName}.zip" "https://codecrafters-coderbot-datasets.s3.amazonaws.com/${datasetName}.zip"
+else
+    wget -O "tmp/datasets/${datasetName}.zip" "https://codecrafters-coderbot-datasets.s3.amazonaws.com/${datasetName}.zip"
+fi
+
 unzip "tmp/datasets/${datasetName}.zip" -d "tmp/datasets/${datasetName}"
 
 sudo chown -R $USER "tmp/datasets/${datasetName}"
