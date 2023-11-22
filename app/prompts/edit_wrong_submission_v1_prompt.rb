5fa2c7dc8c3980faf8fd7897360ee629f3132d7e
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-22 23:41:47 +0000

    Update EditWrongSubmissionV1Prompt class in app/prompts/edit_wrong_submission_v1_prompt.rb

--- app/prompts/edit_wrong_submission_v1_prompt.rb
+++ app/prompts/edit_wrong_submission_v1_prompt.rb
@@ -6,8 +6,7 @@ class EditWrongSubmissionV1Prompt < BasePrompt
     context.result = chat(
       [{role: "user", content: user_message(context.stage, context.language, context.original_code, context.test_runner_output)}],
       seed: context.seed,
-      should_stream: false,
-      request_timeout: 240
+      should_stream: false
     )
   end
 
