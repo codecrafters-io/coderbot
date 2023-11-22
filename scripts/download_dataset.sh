9e8fb04679fac2eaed5c0ad198f24170b562db82
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-22 21:10:16 +0000

    Add chown command to set ownership of downloaded dataset

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -8,3 +8,5 @@ rm -rf "tmp/datasets/${datasetName}"
 rm -rf "tmp/datasets/${datasetName}.zip"
 wget -O "tmp/datasets/${datasetName}.zip" "https://codecrafters-coderbot-datasets.s3.amazonaws.com/${datasetName}.zip"
 unzip "tmp/datasets/${datasetName}.zip" -d "tmp/datasets/${datasetName}"
+
+chown -R $USER:$USER "tmp/datasets/${datasetName}"
