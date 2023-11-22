b8b0731fa338cb748924aeb6b7d030aeca1339e6
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-22 16:08:42 +0000

    Update Gemfile and Gemfile.lock, add migration and schema files, and create a script for running a solver.

---
+++ db/migrate/20231122160449_add_models.rb
@@ -0,0 +1,15 @@
+class AddModels < ActiveRecord::Migration[7.0]
+  def change
+    create_table :solvers, id: :uuid do |t|
+      t.string :status, null: false
+      t.string :repository_clone_url, null: false
+      t.string :last_submission_commit_sha, null: false
+      t.string :last_successful_submission_commit_sha
+      t.string :language_slug, null: false
+      t.string :course_slug, null: false
+      t.string :course_stage_slug, null: false
+      t.string :logstream_url, null: false
+      t.timestamps
+    end
+  end
+end
---
+++ scripts/run_solver.rb
@@ -0,0 +1,8 @@
+Solver.create!(
+  repository_clone_url: "dummy",
+  last_submission_commit_sha: "dummy",
+  last_successful_submission_commit_sha: "dummy",
+  language_slug: "dummy",
+  course_slug: "dummy",
+  logstream_url: "dummy"
+)
