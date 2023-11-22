f6c452c74dd2317d1727745651e78025bd5c600f
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-22 21:14:34 +0000

    Update dataset download script

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -13,6 +13,6 @@ else
     wget -O "tmp/datasets/${datasetName}.zip" "https://codecrafters-coderbot-datasets.s3.amazonaws.com/${datasetName}.zip"
 fi
 
-unzip "tmp/datasets/${datasetName}.zip" -d "tmp/datasets/${datasetName}"
+unzip -q "tmp/datasets/${datasetName}.zip" -d "tmp/datasets/${datasetName}"
 
 sudo chown -R $USER "tmp/datasets/${datasetName}"
