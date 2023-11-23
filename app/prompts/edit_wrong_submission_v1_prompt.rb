593dd80ed889f3fa657716c7014a4fe12f445b28
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-23 01:10:04 +0000

    Fix bug in EditWrongSubmissionV1Prompt class

--- app/prompts/edit_wrong_submission_v1_prompt.rb
+++ app/prompts/edit_wrong_submission_v1_prompt.rb
@@ -46,18 +46,14 @@ class EditWrongSubmissionV1Prompt < BasePrompt
 
       Your goal is to fix the user's code so that it passes the stage.
 
-      First, think through what the bug might be. Then, come up with a plan to fix the bug. Once you have a plan, implement it by editing the user's code. Print the FULL contents of the
-      edited file delimited by triple backticks.
+      First, think through what the bug might be. Then, print out a plan to fix the bug. Once you have the plan, implement it by editing
+      the user's code. Print the FULL contents of the edited file delimited by triple backticks.
 
       Here are some rules to follow:
 
-      * Try to keep your changes minimal, but ensure the solution is complete.
+      * Keep your changes minimal. Only make minor edits, don't rewrite large portions of the code.
       * Add comments to explain your changes, and don't remove existing comments unless they're incorrect or outdated.
-      * You can remove debug print statements if you think they're unnecessary to pass the stage.
       * If you see obvious mistakes, fix them. The user is a beginner, so their code might contain mistakes that you wouldn't typically make.
-      * Try to simplify code where possible, don't overcomplicate things. If a block of code looks too convoluted, it's probably wrong and needs to be redone.
-      * Write production quality code that you think would be acceptable in a real world codebase. Don't write code that you think is hacky and only passes the current stage, unless the stage instructions mention otherwise.
-      * Be wary of code duplication, you might be able to solve the problem better by using recursion or re-using another function.
       * IMPORTANT: Print the FULL contents of the edited file delimited by triple backticks. Don't print partial contents of the file.
     PROMPT
   end
