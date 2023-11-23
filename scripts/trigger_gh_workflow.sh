799b4f30e31ae957a63105b344c11166b26b4f7a
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-23 04:55:44 +0000

    Update trigger_gh_workflow.sh script

--- scripts/trigger_gh_workflow.sh
+++ scripts/trigger_gh_workflow.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 datasetName=$1
-gh workflow run test-dataset.yml -F commit_sha=$(shell git rev-parse HEAD) -F "dataset_name=${datasetName}" -F branch_name="$(git branch --show-current)"
+gh workflow run test-dataset.yml -F commit_sha=$(git rev-parse HEAD) -F "dataset_name=${datasetName}" -F branch_name="$(git branch --show-current)"
 echo $(shell gh run list --workflow=test-dataset.yml --json url | jq -r ".[0].url")
