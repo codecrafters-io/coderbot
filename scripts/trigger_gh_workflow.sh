dfd17378fda84443548838ca49366492c42654c0
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-23 04:58:53 +0000

    Update trigger_gh_workflow.sh to open the GitHub Actions page after running the workflow.

--- scripts/trigger_gh_workflow.sh
+++ scripts/trigger_gh_workflow.sh
@@ -2,4 +2,4 @@
 set -e
 datasetName=$1
 gh workflow run test-dataset.yml -F commit_sha=$(git rev-parse HEAD) -F "dataset_name=${datasetName}" -F branch_name="$(git branch --show-current)"
-echo $(shell gh run list --workflow=test-dataset.yml --json url | jq -r ".[0].url")
+open https://github.com/codecrafters-io/coderbot/actions
