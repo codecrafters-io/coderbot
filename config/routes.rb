f15433056023f04d5cba9ee87582c8d4f715eab1
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-12-15 21:24:06 +0000

    Add active_job url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+require "resque/server"
+
 Rails.application.routes.draw do
   root "site#index"
 
   resources :autofix_requests, only: [:create, :show]
+
+  mount Resque::Server.new, at: "/admin/resque"
 end
