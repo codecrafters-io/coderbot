f82224ea7b381ddbf0bbc49bfbbc9f251e3d4c11
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-12-16 22:37:51 +0000

    Update Sentry configuration to customize traces sampling rate.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,9 +6,33 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
+  # config.traces_sample_rate = 1.0
 
-  # config.traces_sampler = lambda do |context|
-  #   true
-  # end
+  config.traces_sampler = lambda do |sampling_context|
+    # if this is the continuation of a trace, just use that decision (rate controlled by the caller)
+    unless sampling_context[:parent_sampled].nil?
+      next sampling_context[:parent_sampled]
+    end
+
+    rack_env = sampling_context[:env]
+    transaction_context = sampling_context[:transaction_context]
+    op = transaction_context[:op]
+    transaction_name = transaction_context[:name]
+    http_method = rack_env ? rack_env["REQUEST_METHOD"] : "n/a"
+
+    case op
+    when /resque/
+      0.1
+    when /websocket/
+      0.1
+    when /http/
+      if transaction_name.eql?("/") && http_method.eql?("GET")
+        0 # ignore / requests
+      else
+        1 # If we haven't decided to sample by now, let it through!
+      end
+    else
+      1 # Let all other requests through, add more clauses if needed
+    end
+  end
 end
