f14ae6bcadcdf7c44d288936a7a8c10e3d3eb2ff
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-01-16 22:54:20 +0000

    Update Resque configuration to use Redis URL from environment variable if present.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,9 +3,11 @@ require "resque/scheduler/server"
 
 require_relative "../../app/jobs/scheduled_job_wrapper"
 
-Resque.redis = Redis.new(
-  url: ENV["ACTIVE_JOB_REDIS_URL"],
-  ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
-)
+if ENV["ACTIVE_JOB_REDIS_URL"].present?
+  Resque.redis = Redis.new(
+    url: ENV["ACTIVE_JOB_REDIS_URL"],
+    ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
+  )
 
-Resque.schedule = ScheduledJobWrapper.wrap(YAML.load_file("config/resque_schedule.yml"))
+  Resque.schedule = ScheduledJobWrapper.wrap(YAML.load_file("config/resque_schedule.yml"))
+end
